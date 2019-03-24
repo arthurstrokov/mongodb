e2e954d62e09ea11a8fb34b33b8e1dc4ac59d960
arthurstrokov <arthurstrokov@gmail.com>
2019-03-24 19:08:47 +0300

    up add lombok

--- src/main/java/com/gmail/arthurstrokov/mongodb/MongodbApplication.java
+++ src/main/java/com/gmail/arthurstrokov/mongodb/MongodbApplication.java
@@ -1,15 +1,9 @@
 package com.gmail.arthurstrokov.mongodb;
 
-import com.mongodb.MongoClient;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import org.bson.Document;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 public class MongodbApplication implements CommandLineRunner {
